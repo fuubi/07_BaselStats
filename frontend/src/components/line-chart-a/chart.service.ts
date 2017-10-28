3d7058234f7b31ab47ad593d29dd2a94e88fcfdc
Fabrizio <fabrizio.parrillo@fhnw.ch>
2017-10-28 21:06:15 +0200

    create color automaticlly

--- frontend/src/components/line-chart-a/chart.service.ts
+++ frontend/src/components/line-chart-a/chart.service.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@angular/core';
-import { LineChartData } from './model';
+import { LineChartData, LineChartColors } from './model';
 import { Observable } from 'rxjs';
 
 @Injectable()
 export class ChartService {
     private lineChartData: Observable<LineChartData[]>;
     private lineChartLabels: Observable<string[]>;
-    public lineChartColors: any[] = LINE_CHART_COLOR;
+    public lineChartColors: any[];
 
     constructor() {
         this.lineChartLabels =
@@ -19,6 +19,8 @@ export class ChartService {
                 {data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B'},
                 {data: [18, 48, 77, 9, 100, 27, 40], label: 'Series C'}
             ]);
+
+        this.lineChartColors = this.getChartColor(3);
     }
 
     public getLineChartData() {
@@ -32,34 +34,25 @@ export class ChartService {
     public getChartColorShema() {
         return this.lineChartColors;
     }
+
+    private getChartColor(count: Number) {
+        let lineChartColors: LineChartColors[] = [];
+        for (let i = 0; i < count; i++) {
+            let h: string = String(i / count);
+            let lc: LineChartColors = {
+                backgroundColor: 'hsla(' + h + '0, 52%, 66%, 0.3)',
+                borderColor: 'hsla(' + h + ', 52%, 66%, 1)',
+                pointBackgroundColor: 'hsla(' + h + ', 52%, 66%, 1)',
+                pointBorderColor: '#fff',
+                pointHoverBackgroundColor: '#fff',
+                pointHoverBorderColor: 'hsla(' + h + ', 52%, 66%, 1)'
+            };
+
+            lineChartColors.push(lineChartColors);
+        }
+    }
 }
 
 
-let LINE_CHART_COLOR: Array<any> = [
-    { // grey
-        backgroundColor: 'rgba(148,159,177,0.2)',
-        borderColor: 'rgba(148,159,177,1)',
-        pointBackgroundColor: 'rgba(148,159,177,1)',
-        pointBorderColor: '#fff',
-        pointHoverBackgroundColor: '#fff',
-        pointHoverBorderColor: 'rgba(148,159,177,0.8)'
-    },
-    { // dark grey
-        backgroundColor: 'rgba(77,83,96,0.2)',
-        borderColor: 'rgba(77,83,96,1)',
-        pointBackgroundColor: 'rgba(77,83,96,1)',
-        pointBorderColor: '#fff',
-        pointHoverBackgroundColor: '#fff',
-        pointHoverBorderColor: 'rgba(77,83,96,1)'
-    },
-    { // grey
-        backgroundColor: 'rgba(148,159,177,0.2)',
-        borderColor: 'rgba(148,159,177,1)',
-        pointBackgroundColor: 'rgba(148,159,177,1)',
-        pointBorderColor: '#fff',
-        pointHoverBackgroundColor: '#fff',
-        pointHoverBorderColor: 'rgba(148,159,177,0.8)'
-    }
-];
 
 
--- frontend/src/components/line-chart-a/model.ts
+++ frontend/src/components/line-chart-a/model.ts
@@ -2,3 +2,12 @@ export interface LineChartData {
     data: number[];
     label: string;
 }
+
+export interface LineChartColors {
+    backgroundColor: string;
+    borderColor: string;
+    pointBackgroundColor: string;
+    pointBorderColor: string;
+    pointHoverBackgroundColor: string;
+    pointHoverBorderColor: string;
+}
