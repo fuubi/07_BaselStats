b2373a2b70b1584845a7b2510bfffabf55f37cb1
Fabrizio <fabrizio.parrillo@fhnw.ch>
2017-10-28 13:27:29 +0200

    created search component

---
+++ frontend/src/components/search/search.ts
@@ -0,0 +1,48 @@
+import { Component } from '@angular/core';
+
+/**
+ * Generated class for the SearchComponent component.
+ *
+ * See https://angular.io/api/core/Component for more info on Angular
+ * Components.
+ */
+@Component({
+  selector: 'search',
+  templateUrl: 'search.html'
+})
+export class SearchComponent {
+  autocompleteItems;
+  autocomplete;
+
+  text: string;
+
+  constructor() {
+    this.autocompleteItems = [];
+    this.autocomplete = {
+      query: ''
+    };
+    this.text = 'Hello World';
+  }
+
+  updateSearch(): void {
+
+    console.log(this.autocomplete.query)
+
+    if (this.autocomplete.query == '') {
+      this.autocompleteItems = [];
+      return;
+    }
+
+/*    let me = this;
+    this.service.getPlacePredictions({ input: this.autocomplete.query, componentRestrictions: {country: 'TH'} }, function (predictions, status) {
+      me.autocompleteItems = [];
+      me.zone.run(function () {
+        predictions.forEach(function (prediction) {
+          me.autocompleteItems.push(prediction.description);
+        });
+      });
+    });*/
+
+  }
+
+}
