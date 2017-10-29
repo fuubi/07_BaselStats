62b5a97a66dcf66472aea12cf5bd1719f89bc77b
Fabrizio <fabrizio.parrillo@fhnw.ch>
2017-10-29 02:50:19 +0100

    chart works

--- frontend/src/components/line-chart-a/chart.service.ts
+++ frontend/src/components/line-chart-a/chart.service.ts
@@ -2,11 +2,17 @@ import { Injectable } from '@angular/core';
 import { LineChartData, LineChartColors } from './model';
 import { Observable, Subject } from 'rxjs';
 import { SearchService } from '../search/search.service';
+import { multi } from './data';
 
 @Injectable()
 export class ChartService {
-
+    public data: Subject<any>  = new Subject();
     constructor(private searchService: SearchService) {
+        this.searchService.queriedItems
+            .asObservable()
+            .subscribe(d => {
+                this.data.next(multi)
+            });
 
     }
 }
--- frontend/src/components/line-chart-a/line-chart-a.ts
+++ frontend/src/components/line-chart-a/line-chart-a.ts
@@ -1,14 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ChangeDetectorRef } from '@angular/core';
 import { ChartService } from './chart.service';
 import { single, multi } from './data';
+import { BaseChartDirective } from 'ng2-charts';
+import { LineChartComponent } from '@swimlane/ngx-charts';
 
 @Component({
     selector: 'line-chart-a',
     templateUrl: 'line-chart-a.html'
 })
 export class LineChartAComponent {
-    single: any[]  = single;
-    multi: any[]  = multi;
+    single: any[] = single;
+    multi: any[] = multi;
 
     view: any[] = [700, 400];
 
@@ -30,8 +32,33 @@ export class LineChartAComponent {
     autoScale = true;
 
 
-    constructor(private chartService: ChartService) {
+    constructor(private chartService: ChartService,
+                private ref: ChangeDetectorRef) {
 
+        this.chartService.data.asObservable()
+            .subscribe(a => {
+                console.log("allo")
+                console.log(this.multi)
+                this.multi.push({
+                    "name": "Gey",
+                    "series": [
+                        {
+                            "name": "2010",
+                            "value": 700000
+                        },
+                        {
+                            "name": "2011",
+                            "value": 8940000
+                        }
+                    ]
+                });
+                console.log("after")
+                this.multi = [...multi];
+                this.ref.detectChanges();
+            });
+
+
+        console.log(this.chart)
 
     }
 
